c7484d584f40ef357e76194daabc6ddd68937431
Anton <chehob@gmail.com>
2018-08-07 15:13:25 +0400

    скрипт на взаимодействие с фотографиями

---
+++ wwwroot/scripts/image-ui.js
@@ -0,0 +1,24 @@
+﻿$('#photoDiv').on('click', function () {
+	$('#photoFile').click();
+});
+
+$("#photoFile").change(function () {
+	const inputElement = $("#photoFile");
+	const errorElement = $("#fileError");
+	errorElement.html("");
+	const file = this.files[0];
+	const fileSize = file.size;
+	if (fileSize > 300 * 1024) {
+		errorElement.html("Размер файла не может превышать 300KB");
+		inputElement.wrap('<form>').parent('form').trigger('reset');
+		inputElement.unwrap();
+		return;
+	}
+	const reader = new FileReader();
+	reader.onloadend = function () {
+		$("#photoDiv").css("background-image", `url(${this.result})`);
+	}
+	if (file) {
+		reader.readAsDataURL(file);
+	}
+});
