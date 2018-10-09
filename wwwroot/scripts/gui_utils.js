ae3b3f7693742d3023c7c61d5738748bf32bc1ee
Anton <chehob@gmail.com>
2018-10-09 11:32:41 +0300

    OfficeBill update

--- wwwroot/scripts/gui_utils.js
+++ wwwroot/scripts/gui_utils.js
@@ -106,3 +106,9 @@ function updateBalance(ajaxContext) {
     const $div = $("#dvUserBalance");
     $div.load($div.data("url"));
 }
+
+function numberWithSpaces(x) {
+	var parts = x.toString().split(".");
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	return parts.join(".");
+}
